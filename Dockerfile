396789b7a80230ef7b03a64a67fa761bdee4165a
pakawat116688 <pakawatpenpoom@gmail.com>
2024-08-30 11:12:55 +0700

    add code & Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+# Build stage
+FROM golang:1.20-alpine as build
+WORKDIR /app
+COPY go.mod go.sum ./
+RUN go mod download
+COPY . .
+RUN CGO_ENABLED=0 GOOS=linux go build -o app .
+# Deploy stage
+FROM gcr.io/distroless/base-debian11
+COPY --from=build /app/app /app/app
+EXPOSE 3000
+ENTRYPOINT ["/app/app"]
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+
+	api := fiber.New()
+
+	api.Get("/", func(c *fiber.Ctx) error {
+
+		hostname, err := os.Hostname()
+		if err != nil {
+			fmt.Println("Error, Cannot Get Host Name")
+			return c.Status(500).JSON(err)
+		}
+
+		ip := c.IP()
+
+		fmt.Println("Send Request From Path / Success")
+		return c.JSON(fiber.Map{
+			"Hostname":      hostname,
+			"IP":            ip,
+			"Message":       "Test CICD From Go",
+			"Version Image": "v0.1",
+		})
+
+	})
+
+	api.Get("/p1", func(c *fiber.Ctx) error {
+
+		fmt.Println("Send Request From Path / Success")
+		return c.JSON(fiber.Map{
+			"Message":       "Path /p1",
+			"Version Image": "v0.1",
+		})
+
+	})
+
+	api.Listen(":3000")
+}
